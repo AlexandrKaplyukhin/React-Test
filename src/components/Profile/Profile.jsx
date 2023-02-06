30240560cd8295d7de50f24610fa268d2b1031ed
AlexandrKaplyukhin <kaplyukhin7@gmail.com>
2023-02-07 01:34:31 +0300

    second commit

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from  './Profile.module.css'
+import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
@@ -10,18 +11,7 @@ const Profile = () => {
         <div className={s.item}>
           ava + d
           </div>    
-          <div className={s.item}>
-            My Post
-            <div className={s.item}>
-              new post 
-            </div>
-            <div className={s.item}>
-              post 1
-            </div>
-            <div className={s.item}>
-              post 2
-            </div>
-          </div>
+          <MyPosts />
       </div>
     )
 }
